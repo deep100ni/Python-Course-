8bfbc96a9710200969a8dd867361e7283fc77748
deep100ni <deep100ni13@gmail.com>
2024-07-30 15:06:42 +0530

    update Non Negative Number.py

---
+++ Non Negative Number.py
@@ -0,0 +1,8 @@
+# To input a non negative Number 
+
+n = eval(input("Enter non-negative number : "))
+while(n<0): 
+  n = eval(input("Enter non-negative number : "))
+else: 
+  print("Thank you")
+  
